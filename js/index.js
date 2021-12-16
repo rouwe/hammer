bc80c4796b3b5ca91ce8dcac47cba56695bf1e0a
rouwe <84689448+rouwe@users.noreply.github.com>
2021-12-16 15:31:46 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
+// Collapsible Nav Bar
+const burger = document.getElementsByClassName('burger')[0];
 
+burger.addEventListener("click", () => {
+    // toggle navigation on & off
+    const topNav = document.getElementsByClassName('top-nav')[0];
+    const isNone = window.getComputedStyle(topNav, null).display;
+    if (isNone == 'none') {
+        topNav.style.display = 'block';
+    } else {
+        topNav.style.display = 'none';
+    }
+});
